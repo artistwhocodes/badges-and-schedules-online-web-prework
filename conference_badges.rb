a18ed05e7ec748ce714dbc649f91dd1badd81a69
Pamela Stacey Alvarez <artistwhocodes@gmail.com>
2019-04-11 02:45:31 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,12 +6,14 @@
   end
 
 def batch_badge_creator(attendees)
-  badges = []
-  attendees.each do |badge|
-  badges.push("Hello, my name is #{badges}.")
+  badges=[]
+  attendees.each do|badge|
+  badges.push("Hello, my name is #{badge}.")
   end
   return badges
 end
+
+
 
 
 #learn --fail-fast
